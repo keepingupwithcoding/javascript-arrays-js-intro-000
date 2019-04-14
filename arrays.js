12abf848c1f53cc75aa4f3f778fd263839aa8b3b
Omphalus Kua <haluskua.one@gmail.com>
2019-04-14 01:23:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -55,5 +55,5 @@ function destructivelyRemoveElementFromEndOfArray(array){
 // -----
  
  function removeElementFromEndOfArray(array){
-  return array.slice(0, chocolateBars.length - 1);
+  return array.slice(0, array.length - 1);
 }
