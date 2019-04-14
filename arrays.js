2cc6a51d2c777cf8d68eca1f46c5d28bced90a86
Omphalus Kua <haluskua.one@gmail.com>
2019-04-14 01:12:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,8 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 
 function addElementToBeginningOfArray (array, element) {
-  return [element, ...array]
-}
+  return [array, ...element]
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
