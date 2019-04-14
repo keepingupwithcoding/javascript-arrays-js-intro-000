c7804fb44061f2e930ea7ff048edda8d051722c2
Omphalus Kua <haluskua.one@gmail.com>
2019-04-14 01:20:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -40,9 +40,8 @@ function accessElementInArray(array, index){
 //  -----
 
 
-function destructivelyRemoveElementFromBeginningOfArray(array){
-  array.shift();
-  return array;
+function removeElementFromBeginningOfArray(array){
+  return chocolateBars.slice(1);
 }
 
 
